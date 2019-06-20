bf7b4413804816ab9d60fe113a8b7e6e42452389
Iev Chhoung Khov <ickhov@ucdavis.edu>
2019-06-20 15:30:24 -0700

    complete Details page

--- CCIA/Controllers/MyCustomersController.cs
+++ CCIA/Controllers/MyCustomersController.cs
@@ -34,11 +34,15 @@ namespace CCIA.Controllers
         // GET: Application/Details/5
         public async Task<IActionResult> Details(int id)
         {
-            // TODO restrict to logged in user.
-            //var orgId = await _dbContext.Contacts.Where(c => c.Id == 1).Select(c => c.OrgId).SingleAsync();
-            // var model = await ClientSeedsViewModel.Create(_dbContext, orgId, id);
-            // return View(model);
-            return View();
+            var orgId = await _dbContext.Contacts.Where(c => c.Id == 1).Select(c => c.OrgId).ToArrayAsync();
+            var model = await _dbContext.MyCustomers.Where(a => a.Id == id)
+                .Include(e => e.State)
+                .Include(e => e.County)
+                .Include(e => e.Country)
+                .Include(e => e.Organization)
+                .FirstOrDefaultAsync();
+
+            return View(model);
         }
 
         // GET: Application/Create
