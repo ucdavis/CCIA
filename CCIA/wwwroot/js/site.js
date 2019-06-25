0922c1915891146a43a86f4debd543575ebc96c0
James Cubbage <jscubbage@ucdavis.edu>
2019-06-25 12:40:03 -0700

    fix conflicts

--- CCIA/wwwroot/js/site.js
+++ CCIA/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function insertHiddenInput(name, insertVal, formId) {
+    var inputInsert = $("<input>")
+        .attr("type", "hidden")
+        .attr("name", name).val(insertVal);
+    $(formId).append(inputInsert); 
+}
