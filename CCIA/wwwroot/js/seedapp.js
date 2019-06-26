da181bba16ff6e84dfc8a2ae032ed06fbac3c346
Sam Waters <srwaters@ucdavis.edu>
2019-06-26 16:57:10 -0700

    more seed app updates
    - made labels of radio buttons clickable
    - moved searchVarieties out to site.js
    - made second planting stock variety search
    - fixed country dropdown to bind to model
    - improved styling on radio buttons
    - improved styling on page header (app type and grower name)
    - moved javascript from createseedapplication to an external file, seedapp.js

---
+++ CCIA/wwwroot/js/seedapp.js
@@ -0,0 +1,171 @@
+const spinner_div = `<div class="text-center"><div class="spinner-border text-center" role="status"><span class="sr-only">Loading...</span></div></div>`
+var form = document.getElementById("seedApplication")
+let appTypeId = 1;
+// The index of the next Field History entry to be added to the form
+let fhEntryId = 0;
+// How many total Field History entries are in the form currently
+let fhEntryCount = 0;
+let secondPsRendered = false;
+let fhIndices = [0, 0, 0];
+
+$('#seedApplication').submit (function(e) {
+    insertHiddenInput("growerId", growerId, "seedApplication");
+    
+    // Insert hidden input for fhEntryId to let the server know how many fieldhistory records we're submitting
+    // Used in order to aid server-side validation on re-rendering of form
+    insertHiddenInput("fhEntryId", fhEntryId, "seedApplication");
+
+    return true;
+});
+
+// Click listener for first variety search button
+$('#variety-search').on('click', function(e) {
+    e.preventDefault();
+    searchVarieties("variety-dropdown", "variety", "selectVariety");
+});
+
+// Called when clicking a variety name in the first variety dropdown
+function selectVariety(e) {
+    insertHiddenInput("SelectedVarietyId", e.getAttribute("value"), "seedApplication");
+    document.getElementById("variety").value = e.innerHTML;
+
+    // Reset dropdown to be spinner for next search
+    showSpinner("variety-dropdown");
+
+    loadFormRemainder(e.getAttribute("value"), e.getAttribute("innerText"));
+}
+
+// Called when clicking a variety name in the second planting stock variety dropdown
+function selectPs2Variety(e) {
+    // Add hidden input of selected variety's id
+    insertHiddenInput("PlantingStocks[1].OfficialVarietyId", e.getAttribute("value"), "seedApplication");
+    document.getElementById("ps2-variety").value = e.innerHTML;
+}
+
+/* Load the rest of the form after selecting (or entering) a variety*/
+function loadFormRemainder(varietyId, varietyName) {
+    showSpinner("form-remainder");
+    /* If something is inside form-remainder div, clear it out */
+    $("#form-remainder")
+        .load("/Application/GetPartial?folder=Seed&partialName=SeedAppPartial&orgId="+orgId+"&appTypeId="+appTypeId, (response, status, xhr) => {
+                if ( status == "error" ) {
+                    var msg = "Sorry, the following error occurred while loading the remainder of the form: ";
+                    $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
+                }
+                else {
+                    // Set text in planting stock 1's variety to be what we selected above
+                    document.getElementById("ps1-variety").value = document.getElementById("variety").value;
+                    // Create hidden input for planting stock 1's variety id
+                    insertHiddenInput("PlantingStocks[0].OfficialVarietyId", varietyId, "seedApplication");
+                }
+        });
+}
+
+// Takes a parent element as a parameter, and places a centered bootstrap spinner inside
+function showSpinner(parentId) {
+    $(`#${parentId}`)[0].innerHTML = spinner_div;
+}
+
+/* Adds red outline to invalid text boxes on loading after failed form submission */
+$(document).ready(() => {
+    $('.field-validation-error').parents('.form-group').addClass('has-error');
+});
+
+function addNewFieldHistory(e, appTypeId) {
+    e.preventDefault();
+    // Populate div with an additional field history partial
+    fhEntryId = findAvailableFhIndex();
+    if (fhEntryId == -1) {
+        return;
+    }
+    let fh_entry_div = `#fh-entry-${fhEntryId}`;
+    // Show the section
+    document.getElementById("fh-entry-"+fhEntryId).classList.remove("hidden");
+    showSpinner("fh-entry-"+fhEntryId);
+    $(fh_entry_div)
+        .load("/Application/GetPartial?folder=Shared&partialName=_FieldHistoryEntry&orgId="+orgId+"&appTypeId="+appTypeId+"&fhEntryId="+fhEntryId, (response, status, xhr) => {
+                if ( status == "error" ) {
+                    var msg = "Sorry, there was an error loading the remainder of this form: ";
+                    $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
+                }
+                else {
+                    // Mark that id as used
+                    fhIndices[fhEntryId] = 1;
+
+                    // Click handler for removing additional field history entry
+                    document.getElementById(fhEntryId).onclick = (e) => {
+                        e.preventDefault();
+                        // Grabs ID from closest parent section -- the parent section containing the "X" button
+                        let idToRemove = parseInt(e.target.closest("button").id);
+                        // Hide the section
+                        document.getElementById("fh-entry-"+idToRemove).classList.add("hidden");
+                        // Mark this entry as unused
+                        if (fhEntryCount === 3) {
+                            // Re-enable button to add new entry
+                            document.getElementById("add-fieldhistory").disabled = false;
+                        }
+                        idToRemove = parseInt(e.target.closest("button").id);
+                        fhIndices[idToRemove] = 0;
+                        e.preventDefault();
+                        fhEntryCount--;
+                        removeFhEntryById(idToRemove);                              
+                    }
+                    fhEntryCount++;
+                }
+                // TODO: change to pull from model's MaxFieldHistoryEntries property instead of using "3"
+                if (fhEntryCount === 3)
+                {
+                    document.getElementById("add-fieldhistory").disabled = true;
+                }
+        });
+}
+
+function findAvailableFhIndex() {
+    for (let i=0; i<fhIndices.length; i++) {
+        // 0 = free, 1 = used
+        if (fhIndices[i] === 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function addSecondPs(e) {
+    e.preventDefault();
+    showSpinner("second-ps");
+    let appTypeId = 1;
+    if (secondPsRendered) {
+        return;
+    }
+    // Populate div with second planting stocks partial
+    $("#second-ps")
+        .load("/Application/GetPartial?folder=Seed&partialName=_SecondPlantingStock&orgId="+orgId+"&appTypeId="+appTypeId, (response, status, xhr) => {
+                if ( status == "error" ) {
+                    var msg = "Sorry, there was an error loading the remainder of this form: ";
+                    $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
+                }     
+                else {
+                    secondPsRendered = true;
+                    let addSeconPsBtn = document.getElementById("add-second-ps");
+                    addSeconPsBtn.disabled = true;
+
+                    // Click handler for removing the second planting stocks record
+                    document.getElementById("remove-ps2").onclick = (e) => {
+                        secondPsRendered = false;
+                        e.preventDefault();
+                        removeSecondPSEntry();
+                        addSeconPsBtn.disabled = false;
+                    }
+                }           
+        });
+}
+
+// Removes contents of a field history section, while keeping the section tags intact
+function removeFhEntryById(id) {
+    let fhEntrySection = `#fh-entry-${id}`;
+    $(fhEntrySection).empty();
+}
+
+function removeSecondPSEntry(){
+    $("#second-ps").empty();
+}
--- CCIA/wwwroot/js/site.js
+++ CCIA/wwwroot/js/site.js
@@ -1,7 +1,57 @@
-﻿function insertHiddenInput(name, insertVal, formId) {
+﻿/**
+ * @param  {} name
+ * @param  {} insertVal
+ * @param  {} formId
+ */
+function insertHiddenInput(name, insertVal, formId) {
     let inputInsert = document.createElement("input");
     inputInsert.setAttribute("type", "hidden");
     inputInsert.setAttribute("name", name);
     inputInsert.setAttribute("value", insertVal.toString());
     document.getElementById(formId).appendChild(inputInsert); 
+}
+
+function searchVarieties(dropdownId, varietyInputId, selectVarietyCallback) {
+    // Display error if user tries to search for variety before selecting crop
+    let crop = document.getElementsByName("CropId")[0];
+    let cropText = crop.options[crop.selectedIndex].text;
+    let cropId = crop.options[crop.selectedIndex].value;
+    let varietyName = document.getElementById(varietyInputId).value;
+    if (cropText === "") { 
+        $("#cropAlert").modal('show');
+        return;
+    }
+
+    if (varietyName === "") {
+        $("#emptyVarAlert").modal('show');
+        return;
+    }
+
+    let data = { 
+        name: varietyName,
+        cropId: cropId
+    };
+    let vs = document.getElementById(dropdownId);
+    // Take text in input box and autofill input with the variety name that most closely matches it
+    $.ajax({
+        type: "GET",
+        url: "/Application/FindVariety",
+        data: data,
+        success: function(res) {
+            vs.innerHTML = "";
+            if (res.length === 0) {
+                $("#varAlert").modal('show');
+                loadFormRemainder();
+            }
+            // Populate dropdown with list of varieties
+            res.forEach((el) => {
+                vs.innerHTML += `<a class="dropdown-item" onclick=${selectVarietyCallback}(this)
+                                id=${el.varOffId} value=${el.varOffId}>${el.varOffName}</a>`;
+            })
+        },
+        error: function(res) {
+            console.log(res);
+            alert("There was an error processing the request");
+        }
+    });
 }
