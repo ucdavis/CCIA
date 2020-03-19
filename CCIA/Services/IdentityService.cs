538e13475812e2893e7308a30ed21c58c63ead86
James Cubbage <jscubbage@ucdavis.edu>
2020-03-19 16:43:35 -0700

    pulling from employee table

---
+++ CCIA/Services/IdentityService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using CCIA.Models;
+
+
+namespace CCIA.Services
+{
+    public interface IIdentityService
+    {
+       
+        Task<CCIAEmployees> GetByKerberos(string kerb);
+       
+    }
+
+    public class IdentityService : IIdentityService
+    {       
+        private readonly CCIAContext _context;
+       
+
+
+        public IdentityService(CCIAContext context)
+        {           
+            _context = context;            
+        }
+
+        
+
+        public async Task<CCIAEmployees> GetByKerberos(string kerb)
+        {
+            var rtValue = await _context.CCIAEmployees.Where(e => e.KerberosId == kerb).FirstOrDefaultAsync();
+            return rtValue;
+        }
+
+       
+    }
+}
--- CCIA/Startup.cs
+++ CCIA/Startup.cs
@@ -24,6 +24,7 @@ namespace CCIA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IIdentityService, IdentityService>();
             services.AddMvc();
 
             services.AddDbContext<CCIAContext>();
@@ -50,26 +51,26 @@ namespace CCIA
 
                         if (string.IsNullOrWhiteSpace(kerb)) return;
 
-                        // var identityService = services.BuildServiceProvider().GetService<IIdentityService>();
+                        var identityService = services.BuildServiceProvider().GetService<IIdentityService>();
 
-                        // var user = await identityService.GetByKerberos(kerb);
+                        var user = await identityService.GetByKerberos(kerb);
 
-                        // if (user == null)
-                        // {
-                        //     throw new InvalidOperationException("Could not retrieve user information from IAM");
-                        // }
+                        if (user == null)
+                        {
+                            return;
+                        }
                         
 
-                        // identity.RemoveClaim(identity.FindFirst(ClaimTypes.NameIdentifier));
-                        // identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                        identity.RemoveClaim(identity.FindFirst(ClaimTypes.NameIdentifier));
+                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
 
-                        // identity.RemoveClaim(identity.FindFirst(ClaimTypes.Name));
-                        // identity.AddClaim(new Claim(ClaimTypes.Name, user.Id));
+                        identity.RemoveClaim(identity.FindFirst(ClaimTypes.Name));
+                        identity.AddClaim(new Claim(ClaimTypes.Name, user.Id));
 
-                        // identity.AddClaim(new Claim(ClaimTypes.GivenName, user.FirstName));
-                        // identity.AddClaim(new Claim(ClaimTypes.Surname, user.LastName));
-                        // identity.AddClaim(new Claim("name", user.Name));
-                        // identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+                        identity.AddClaim(new Claim(ClaimTypes.GivenName, user.FirstName));
+                        identity.AddClaim(new Claim(ClaimTypes.Surname, user.LastName));
+                        identity.AddClaim(new Claim("name", user.Name));
+                        identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
                         identity.RemoveClaim(identity.FindFirst(ClaimTypes.NameIdentifier));
                         identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, kerb));
                         identity.AddClaim(new Claim(ClaimTypes.Role, "Employee"));
