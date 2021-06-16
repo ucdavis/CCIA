28fad59ee872b3b7e7cfc41f99f848ee1942daa0
James Cubbage <jscubbage@ucdavis.edu>
2021-06-15 17:09:39 -0700

    Variety search done

--- CCIA/Areas/Admin/Models/Variety/VarietySearchViewModel.cs
+++ CCIA/Areas/Admin/Models/Variety/VarietySearchViewModel.cs
@@ -43,15 +43,65 @@ namespace CCIA.Models
                
         public static async Task<AdminVarietySearchViewModel> Create(CCIAContext _dbContext, IFullCallService _helper, AdminVarietySearchViewModel vm)
         {      
-           var var = _helper.FullVariety().AsQueryable(); 
+           var varietySearch = _helper.FullVariety().AsQueryable(); 
+
+           if(vm != null)
+           {
            
-           var = var.Where(v => v.Id == 13381); 
+                // var = var.Where(v => v.Id == 13381); 
+                if(vm.varietyIdToSearch != 0)
+                {
+                    varietySearch = varietySearch.Where(v => v.Id == vm.varietyIdToSearch);
+                }
+                if(!string.IsNullOrWhiteSpace(vm.varietyNameToSearch))
+                {
+                    varietySearch = varietySearch.Where(v => EF.Functions.Like(v.Name, "%" + vm.varietyNameToSearch + "%"));
+                }
+                if(vm.searchCrops != null && vm.searchCrops.Count > 0)
+                {
+                    varietySearch = varietySearch.Where(v => vm.searchCrops.Contains(v.CropId));
+                }
+                if(vm.searchType != null && vm.searchType.Count > 0)
+                {
+                    varietySearch = varietySearch.Where(v => vm.searchType.Contains(v.Type));
+                }
+                if(vm.categoryToSearch != "Both")
+                {
+                    varietySearch = varietySearch.Where(v => v.Category == vm.categoryToSearch);
+                }
+                if(vm.statusToSearch != "Both")
+                {
+                    varietySearch = varietySearch.Where(v => v.Status == vm.statusToSearch);
+                }
+           } else
+           {
+               varietySearch = varietySearch.Where(v => v.Id == -1);
+           }
+
+        //   if(vm.searchType != null && vm.searchType > 0)
+        //   {
+              
+        //   }
+
+        //        if(vm.searchStatus != null && vm.searchStatus.Count > 0)
+        //         {                    
+        //             blendsToFind = blendsToFind.Where(b => vm.searchStatus.Contains(b.Status));
+        //         } 
+        //         if(vm.typeSearchValue != "Any")             
+        //         {
+        //             blendsToFind = blendsToFind.Where(b => b.BlendType == vm.typeSearchValue);
+        //         }
+        //         if(!string.IsNullOrWhiteSpace(vm.conditionerSearchTerm))
+        //         {
+        //             blendsToFind = blendsToFind.Where(b => EF.Functions.Like(b.Conditioner.Name, "%" + vm.conditionerSearchTerm + "%") || b.ConditionerId.ToString() == vm.conditionerSearchTerm);
+        //         }
+
+
            var typesList =  EnumHelper.GetListOfDisplayNames<VarietyTypes>();
-           typesList.Add("Any");
                        
             var model = new AdminVarietySearchViewModel
             {
-                varieties = await var.ToListAsync(), 
+                varieties = await varietySearch.ToListAsync(), 
                 typeOptions = typesList, 
                 crops = await _dbContext.Crops.OrderBy(c => c.Crop).ThenBy(c => c.CropKind).ToListAsync(), 
                 statusOptions = new List<string> {  "Both", "Certified", "Pending"}, 
