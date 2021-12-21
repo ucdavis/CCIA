2856fab072993d8d636aa82875f61b572e6ac338
James Cubbage <jscubbage@ucdavis.edu>
2021-12-21 14:01:28 -0800

    app, blend, bsc next/previous

--- CCIA/Areas/Admin/Controllers/BlendsController.cs
+++ CCIA/Areas/Admin/Controllers/BlendsController.cs
@@ -133,6 +133,18 @@ namespace CCIA.Controllers.Admin
            return View(model);
        } 
 
+        public async Task<IActionResult> Previous(int id)
+        {
+            var previousId = await _dbContext.BlendRequests.Where(x => x.Id < id).OrderBy(x => x.Id).Select(x => x.Id).LastOrDefaultAsync();
+            return RedirectToAction(nameof(Details), new {id = previousId});
+        }
+
+        public async Task<IActionResult> Next(int id)
+        {
+            var previousId = await _dbContext.BlendRequests.Where(x => x.Id > id).OrderBy(x => x.Id).Select(x => x.Id).FirstOrDefaultAsync();
+            return RedirectToAction(nameof(Details), new {id = previousId});
+        }
+
        public async Task<IActionResult> EditVariety(int id)
        {
            var model = await _dbContext.BlendRequests.Where(b => b.Id == id).FirstOrDefaultAsync();
--- CCIA/Areas/Admin/Controllers/BulkSalesCertificatesController.cs
+++ CCIA/Areas/Admin/Controllers/BulkSalesCertificatesController.cs
@@ -45,6 +45,18 @@ namespace CCIA.Controllers.Admin
             return View(model);
         }
 
+        public async Task<IActionResult> Previous(int id)
+        {
+            var previousId = await _dbContext.BulkSalesCertificates.Where(x => x.Id < id).OrderBy(x => x.Id).Select(x => x.Id).LastOrDefaultAsync();
+            return RedirectToAction(nameof(Details), new {id = previousId});
+        }
+
+        public async Task<IActionResult> Next(int id)
+        {
+            var previousId = await _dbContext.BulkSalesCertificates.Where(x => x.Id > id).OrderBy(x => x.Id).Select(x => x.Id).FirstOrDefaultAsync();
+            return RedirectToAction(nameof(Details), new {id = previousId});
+        }
+
         public async Task<IActionResult> Edit(int id)
         {
             var model = await AdminBulkSalesCertificatesEditViewModel.Create(_dbContext, id, _helper);
