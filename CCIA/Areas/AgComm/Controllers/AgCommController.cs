f3853a49e926ec0152aa60c82e3e6ff8689ef706
James Cubbage <jscubbage@ucdavis.edu>
2022-08-15 14:11:08 -0700

    start ag comm; correct the role for client.

--- CCIA/Areas/AgComm/Controllers/AgCommController.cs
+++ CCIA/Areas/AgComm/Controllers/AgCommController.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CCIA.Controllers.Client
-{
-    [Area("Client")]
-    [Authorize(Roles = "Member")]  
-    public class ClientController : SuperController
+namespace CCIA.Controllers
+{    
+    [Area("AgComm")]
+    [Authorize(Roles = "AgComm")]  
+    public class AgCommController : SuperController
     {
     }
 }
