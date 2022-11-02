4ff9a618b0cfabc29d92389a7228b6d9bab5ab05
James Cubbage <jscubbage@ucdavis.edu>
2022-11-01 17:45:03 -0700

    Limit tags for complete stage.

--- CCIA/Areas/Admin/Models/Tags/ProcessViewModel.cs
+++ CCIA/Areas/Admin/Models/Tags/ProcessViewModel.cs
@@ -17,20 +17,31 @@ namespace CCIA.Models
         public List<ProcessTag> Tags { get; set; }
 
         public int SelectedSearch { get; set; }
+
+        public int YearSubmitted { get; set; }
+
+        public List<int> YearsToDisplay { get; set; }
         
         
         public static async Task<ProcessViewModel> Create(CCIAContext _dbContext, ProcessViewModel vm, IFullCallService helper)
         {  
             var stage = (TagStages) vm.SelectedSearch;
+            if(vm.YearSubmitted ==0)
+            {
+                vm.YearSubmitted = CertYearFinder.CertYear;
+            }
             var p0 = new SqlParameter("@stage", System.Data.SqlDbType.VarChar); 
+            var p1 = new SqlParameter("@year", System.Data.SqlDbType.Int);
+            p1.SqlValue = vm.YearSubmitted;
             if(vm.SelectedSearch == 0)
             {                
                 p0.SqlValue = TagStages.Requested;
-                var tags =  await _dbContext.ProcessTag.FromSqlRaw($"EXEC mvc_tags_by_status_blends @stage", p0).ToListAsync();                                         
+                var tags =  await _dbContext.ProcessTag.FromSqlRaw($"EXEC mvc_tags_by_status_blends @stage, @year", p0, p1).ToListAsync();                                         
                 var freshmodel = new ProcessViewModel
                 {
                     Tags = tags,
-                    SelectedSearch = 0                   
+                    SelectedSearch = 0 , 
+                    YearsToDisplay = CertYearFinder.certYearListReverse               
                 };  
                     
                 return freshmodel;
@@ -38,11 +49,12 @@ namespace CCIA.Models
 
             p0.SqlValue = stage.GetDisplayName();
             
-            var model = await _dbContext.ProcessTag.FromSqlRaw($"EXEC mvc_tags_by_status_blends @stage", p0).ToListAsync();
+            var model = await _dbContext.ProcessTag.FromSqlRaw($"EXEC mvc_tags_by_status_blends @stage, @year", p0, p1).ToListAsync();
             var viewModel = new ProcessViewModel
             {
                 Tags = model,
-                SelectedSearch = vm.SelectedSearch                   
+                SelectedSearch = vm.SelectedSearch,
+                YearsToDisplay = CertYearFinder.certYearListReverse                   
             };  
                 
             return viewModel;
