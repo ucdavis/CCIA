dd0e3a2c4be748376b47c70be2a0e3394af12f48
James Cubbage <jscubbage@ucdavis.edu>
2023-01-13 17:24:17 -0800

    Update for library and varieties

--- CCIA/Areas/Admin/Controllers/AdminHomeController.cs
+++ CCIA/Areas/Admin/Controllers/AdminHomeController.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CCIA.Models;
 using Microsoft.AspNetCore.Authorization;
+using CCIA.Services;
 
 namespace CCIA.Controllers
 {
     public class AdminHomeController : AdminController
     {
+        private readonly IFileIOService _fileService;
+
+        public AdminHomeController(IFileIOService fileIOService)
+        {            
+            _fileService = fileIOService;            
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,6 +26,12 @@ namespace CCIA.Controllers
         public IActionResult CookieView()
         {
             return View();
+        }
+
+        public IActionResult Download(string file)
+        {            
+            var contentType = "APPLICATION/octet-stream";            
+            return File(_fileService.DownloadLibrary(file), contentType, file);
         }
        
     }
