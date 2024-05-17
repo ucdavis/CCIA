ff8a9cfaafd0a553baaa5259fabb9dccbe8eb014
James Cubbage <jscubbage@ucdavis.edu>
2024-05-17 13:12:22 -0700

    Allow update pinning map

--- CCIA/Areas/Admin/Models/Applications/NewMapViewModel.cs
+++ CCIA/Areas/Admin/Models/Applications/NewMapViewModel.cs
@@ -38,11 +38,20 @@ namespace CCIA.Models
         }
 
         public static async Task<NewMapViewModel> CreateClient(CCIAContext _dbContext, int id)
-        {  
+        {
+            var appid = new SqlParameter("app_id", id);
+            var link = new SqlParameter
+            {
+                ParameterName = "linked",
+                SqlDbType = System.Data.SqlDbType.Bit,
+                Direction = System.Data.ParameterDirection.Output,
+                Size = 1,
+            };
+            await _dbContext.Database.ExecuteSqlRawAsync($"EXEC mvc_app_check_if_linked_to_iso_map @app_id, @linked output", appid, link);
             var model = new NewMapViewModel
                 {
                    application = await _dbContext.Applications.Where(a => a.Id==id).FirstOrDefaultAsync(),
-                   showLink =  false,                  
+                   showLink = (bool)link.Value,                  
                 };
             
             return model;
